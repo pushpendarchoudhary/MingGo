2f52e9a7d95a548055e6a7843b8f4e2fa43e451e
pushpendarchoudhary <pushpendar012@gmail.com>
2023-12-22 01:10:42 +0530

    21-12-23

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-module.export = app;
+// Route Imports 
+
+const Users = require("./Routes/UserRoutes");
+app.use("/api/v1", Users);
+module.exports = app;
